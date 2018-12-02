dfd9ab926128ffa7a919d463695dfb58d333840c
Asaf Chelouche <asaf.chelouche@gmail.com>
2018-12-02 20:57:26 +0200

    Performance improvements

--- brew.sh
+++ brew.sh
@@ -20,6 +20,7 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
+brew install bash-completion
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
@@ -28,53 +29,12 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Install `wget` with IRI support.
-# brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-# brew install ringojs
-# brew install narwhal
-
 # Install more recent versions of some macOS tools.
 # brew install vim --override-system-vi
 # brew install homebrew/dupes/grep
 # brew install homebrew/dupes/openssh
 # brew install homebrew/dupes/screen
 # brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
 
 # Install other useful binaries.
 # brew install ack
@@ -97,26 +57,26 @@ brew install git
 # brew install vbindiff
 # brew install webkit2png
 # brew install zopfli
-brew install rbenv
-brew install ruby-build
-brew install python3
-brew install node
-brew install opam # for OCaml
-brew install mono # for F#
-brew install scala
-brew install sbt # Scala Build Tool
-brew install mongodb@3.4
+# brew install rbenv
+# brew install ruby-build
+# brew install python3
+# brew install node
+# brew install opam # for OCaml
+# brew install mono # for F#
+# brew install scala
+# brew install sbt # Scala Build Tool
+# brew install mongodb
 brew install redis
-brew install rabbitmq
+# brew install rabbitmq
 
 # Add all kinds of fancy tab auto-completions to bash
-brew install bash-completion
-brew install apm-bash-completion
-brew install gem-completion
-brew install grunt-completion
-brew install rails-completion
-brew install rake-completion
-brew install ruby-completion
+# brew install bash-completion
+# brew install apm-bash-completion
+# brew install gem-completion
+# brew install grunt-completion
+# brew install rails-completion
+# brew install rake-completion
+# brew install ruby-completion
 
 
 # Remove outdated versions from the cellar.
